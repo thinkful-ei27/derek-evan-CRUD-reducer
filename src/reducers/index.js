cebca2f7e8d365370af15509e8560bdbad053de9
Glitch (redux-crud-reducer-starting-point) <none>
2019-01-28 08:44:40 -0800

    drill completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,29 @@
-import {ADD_ITEM, UPDATE_ITEM, DELETE_ITEM} from '../actions';
+import { ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from '../actions';
 
 const initialState = {
-    items: []
+  items: []
 };
 
-export const crudReducer = (state=initialState, action) => {
-    // Add code which handles each action here
-    if (action.type === ADD_ITEM) {
-    }
-    else if (action.type === UPDATE_ITEM) {
-    }
-    else if (action.type === DELETE_ITEM) {
-    }
-    return state;
+export const crudReducer = (state = initialState, action) => {
+  // Add code which handles each action here
+  if (action.type === ADD_ITEM) {
+    return Object.assign({}, state, {
+      items: [...state.items, action.item]
+    });
+  } else if (action.type === UPDATE_ITEM) {
+    return Object.assign({}, state, {
+      items: state.items.map(item => {
+        if (item.id === action.item.id) {
+          return action.item;
+        } else {
+          return item;
+        }
+      })
+    });
+  } else if (action.type === DELETE_ITEM) {
+    return Object.assign({}, state, {
+      items: state.items.filter(item => item.id !== action.item.id)
+    });
+  }
+  return state;
 };
